06003b553b2555b8e2bf6787f2ce753138e1e663
Maykon G. Pedro <maykonglaffite@gmail.com>
2021-10-06 20:21:03 -0300

    finalizando aula 1

--- pratica/02-output.R
+++ pratica/02-output.R
@@ -1,14 +1,21 @@
 library(shiny)
 
 ui <- fluidPage(
+  
+  # texto que aparece antes da exibição do output
   "Um histograma",
+  
+  # defini o nome do output para ser chamado no server
   plotOutput(outputId = "histograma")
 )
 
 server <- function(input, output, session) {
   
   output$histograma <- renderPlot({
+    
+    # aquié onde fica o código R que gera o output
     hist(mtcars$mpg)
+    
   })
   
 }
--- pratica/031-output-input.R
+++ pratica/031-output-input.R
@@ -1,19 +1,38 @@
+
 library(shiny)
 
 ui <- fluidPage(
+  
+  # texto que aparece antes da exibição do output
   "Um histograma",
+  
   selectInput(
+    
+    # nome do input
     inputId = "variavel",
-    label = "Escolha uma variável",
-    choices = names(mtcars)
-  ),
+    
+    # informação adicional para o usuário
+    label = "Escolha uma variavel",
+    
+    # itens para seleção do usuário
+    choices = names(mtcars) 
+    
+    ),
+  
+  # definir o nome do output para ser chamado no server
   plotOutput(outputId = "histograma")
+  
 )
 
 server <- function(input, output, session) {
   
   output$histograma <- renderPlot({
+    
+    # aqui onde fica o código R que gera o output
+    # input$variavel é para puxar a variavel de escolha criada na UI
+    # está dentro de [] pois o resultado é uma string, o nome da coluna, no caso
     hist(mtcars[[input$variavel]])
+    
   })
   
 }
