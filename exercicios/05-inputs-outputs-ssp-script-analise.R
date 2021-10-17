f2767bffee30919559ae22ff89d9246bb8699cec
Maykon G Pedro <maykonglaffite@gmail.com>
2021-10-17 20:07:26 -0300

    add rascunho da análise da base de cirminalidade

--- exercicios/05-inputs-outputs-ssp-script-analise.R
+++ exercicios/05-inputs-outputs-ssp-script-analise.R
@@ -19,8 +19,75 @@ bd_criminalidade <- readr::read_rds("exercicios/dados/ssp.rds")
 bd_criminalidade |> dplyr::glimpse()
 
 
-# gerar base para o plot
+# agrupar todos os crimes em apenas uma coluna
+bd_criminalidade_pivot <- bd_criminalidade |> 
+
+    tidyr::pivot_longer(cols = 6:ncol(bd_criminalidade),
+                        names_to = "crime_cometido",
+                        values_to = "ocorrencias") |> 
+    
+    # retirando vítimas dos crimes
+    dplyr::filter(!stringr::str_detect(crime_cometido, "vit"))
+
+
+bd_criminalidade_pivot |> dplyr::glimpse()
+
+# visualizar quantos itens tem NA em cada coluna
+bd_criminalidade_pivot |> 
+    dplyr::summarise(
+        dplyr::across(
+            .cols = dplyr::everything(),
+            .fns = ~sum(is.na(.x))
+        )
+    ) |> 
+    dplyr::select(
+        where(~.x > 0)
+    )
+
+
+# retirar itens com NA
+bd_criminalidade_pivot <- bd_criminalidade_pivot |> 
+    dplyr::filter(!is.na(delegacia_nome))
+
+
+# gerar base para a tabela
+
+
+if (escolha_muni == TRUE) {
+    base_filtro <- bd_criminalidade_pivot |>
+        dplyr::filter(
+            mes == 1,
+            ano == 2002,
+            municipio_nome == "",
+            # delegacia_nome == "",
+            # regiao_nome == ""
+        ) 
+}
 
 
 
-# plotar gráfico
+base_filtro <- bd_criminalidade_pivot |>
+    dplyr::filter(
+        mes == 1,
+        ano == 2002,
+        # municipio_nome == "",
+        # delegacia_nome == "",
+        # regiao_nome == ""
+    ) 
+    
+
+base_table <- base_filtro |> 
+    dplyr::group_by(crime_cometido) |> 
+    dplyr::summarise(total_ocorrencias = sum(ocorrencias)) |> 
+    dplyr::arrange(desc(total_ocorrencias)) |> 
+    dplyr::rename(
+        "Tipo" = "crime_cometido",
+        "Total de ocorrências" = "total_ocorrencias"
+    )
+
+
+# plotar tabela
+
+
+
+
