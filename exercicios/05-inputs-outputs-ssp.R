dc9ed933dce81197473599cbcf553a968c6ac140
Maykon G Pedro <maykonglaffite@gmail.com>
2021-10-18 21:49:59 -0300

    ajustando ordem da pivotagem

--- exercicios/05-inputs-outputs-ssp.R
+++ exercicios/05-inputs-outputs-ssp.R
@@ -12,21 +12,88 @@ library(shiny)
 
 # Carregar base -----------------------------------------------------------
 bd_criminalidade <- readr::read_rds("dados/ssp.rds")
+bd_criminalidade_pivot |> dplyr::glimpse()
 
 
 # Criar base de escolhas -------------------------------------------------
+meses <- bd_criminalidade |> 
+    dplyr::distinct(mes)
 
+ano <- bd_criminalidade |> 
+    dplyr::distinct(ano)
 
 
 # Criar app ---------------------------------------------------------------
 
 
 ui <- fluidPage(
+    
+    # input 1
+    selectInput(
+        inputId = "mes",
+        label = "Selecione o mês desejado",
+        choices = meses$mes,
+    ),
+    
+    # input 2
+    selectInput(
+        inputId = "ano",
+        label = "Selecione o ano desejado",
+        choices = ano$ano
+    ),
+    
+    "Aluno: Maykon G. Pedro",
+    br(),
+    "Exercício 5 - Inputs-Outputs-SSP",
+    br(),
+    "Script: 022-inputs-outputs-ssp.R",
+    br(),
+    "A tabela exibe a quantidade de ocorrências por crime cometido.",
+    
+    # output
+    tableOutput(outputId = "tabela")
   
 )
 
 server <- function(input, output, session) {
   
+    
+    output$tabela <- renderTable({
+        
+        # faz o filtro primeiro e depois a pivotagem para ficar mais rápido
+        base_filtrada <- bd_criminalidade |> 
+            dplyr::filter(
+                mes == input$mes,
+                ano == input$ano
+                # municipio_nome == "",
+                # delegacia_nome == "",
+                # regiao_nome == ""
+            ) 
+        
+        # agrupar todos os crimes em apenas uma coluna
+        base_tabela <- base_filtrada |> 
+            
+            tidyr::pivot_longer(cols = 6:ncol(bd_criminalidade),
+                                names_to = "crime_cometido",
+                                values_to = "ocorrencias") |> 
+            
+            # retirando vítimas dos crimes
+            dplyr::filter(!stringr::str_detect(crime_cometido, "vit")) |> 
+            dplyr::filter(!is.na(delegacia_nome))
+        
+        
+        base_tabela |> 
+        dplyr::group_by(crime_cometido) |> 
+        dplyr::summarise(total_ocorrencias = sum(ocorrencias)) |> 
+        dplyr::arrange(desc(total_ocorrencias)) |> 
+        dplyr::rename(
+            "Tipo" = "crime_cometido",
+            "Total de ocorrências" = "total_ocorrencias"
+        )
+        
+    })
+        
+    
 }
 
 shinyApp(ui, server)
