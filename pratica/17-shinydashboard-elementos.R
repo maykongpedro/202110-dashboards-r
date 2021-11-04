19d4afca7e44952527f1423710c972d350f5bb2d
Maykon G Pedro <maykonglaffite@gmail.com>
2021-11-03 21:27:43 -0300

    add pratica17 - aula5

---
+++ pratica/17-shinydashboard-elementos.R
@@ -0,0 +1,123 @@
+
+library(shiny)
+library(shinydashboard)
+
+# instalar pacote para acessar bases do curso
+# remotes::install_github("curso-r/basesCursoR")
+
+# carregar base
+# imdb <- basesCursoR::pegar_base("imdb")
+
+ui <- dashboardPage(
+    
+     # cabeçalho
+     dashboardHeader(),
+     
+     # barra lateral
+     dashboardSidebar(
+         sidebarMenu(
+             menuItem(text = "Informações gerais", tabName = "info"),
+             menuItem(text = "Financeiro", tabName = "financeiro"),
+             menuItem(text = "Elenco", tabName = "elenco")
+         )
+     ),
+     
+     # corpo principal
+     dashboardBody(
+         tabItems(
+             # tabitem 1
+             tabItem(
+                 tabName = "info",
+                 
+                 # fluid row 1
+                 fluidRow( # add linha
+                     column( # add coluna
+                         width = 12,
+                         h2("Informações gerais dos filmes") # add título
+                     )
+                 ),
+                 
+                 br(), # pular linha,
+                 
+                 # fluidrow2
+                 fluidRow(
+                     
+                     # info box é um elemento que ja gera uma coluna, ou seja,
+                     # nao posso colocar outra coluna aqui dentro usando column
+                     # se possui o argumento 'width' é quase certo que é uma coluna
+                     infoBoxOutput(outputId = "num_filmes", width = 4),
+                     infoBoxOutput(outputId = "num_diretores", width = 4),
+                     infoBoxOutput(outputId = "num_atores", width = 4)
+                 )
+                 ),
+             
+             # tabitem 2
+             tabItem(tabName = "financeiro"),
+             
+             # tabitem 3
+             tabItem(tabName = "elenco")
+         )
+     )
+    
+)
+
+server <- function(input, output, session) {
+  
+    # carregar base
+    imdb <- basesCursoR::pegar_base("imdb")
+    
+    # output 1 - número de filmes
+    output$num_filmes <- renderInfoBox({
+        
+        # quantidade de filmes na base
+        numero_filmes <- nrow(imdb)
+        
+        # formatar para exibir melhor no dashboard
+        numero_filmes <- prettyNum(numero_filmes, big.mark = ".", decimal.mark = ",")
+  
+        # essa info box pode ser carregada na UI se a base for carregada antes,
+        # porque ela é basicamente um HMTL, caso seja estático
+        infoBox(
+            title = "Número de filmes",
+            value = numero_filmes,
+            color = "orange",
+            
+            # para usar ícones pode ser utilizado os nomes dos ícones disponíveis
+            # no site "https://fontawesome.com/", o que não está em cinza é gratuito
+            icon = icon("film")
+        )
+    })
+    
+    # output 2 - número de diretores
+    output$num_diretores <- renderInfoBox({
+        
+        # calcular quantidade de diretores únicos na base
+        numero_diretores <- ""
+        
+        infoBox(
+            title = "Número de diretores e diretoras",
+            value = numero_diretores,
+            color = "orange",
+            icon = icon("film")
+        )
+        
+    })
+    
+    # output 3 - número de atores e atrizes
+    output$num_atores <- renderInfoBox({
+        
+        # calcular quantidade de atores distintos na base
+        numeros_atores <- ""
+        
+        infoBox(
+            title = "Número de atrizes e atores",
+            value = numeros_atores,
+            color = "orange",
+            icon = icon("film")
+        )
+        
+    })
+    
+}
+
+shinyApp(ui, server)
