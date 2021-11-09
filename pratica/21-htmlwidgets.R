3771e0acb4b2f63eda117a30687f474e07807458
Maykon G Pedro <maykonglaffite@gmail.com>
2021-11-08 23:01:31 -0300

    add construção da ui na pratica21

--- pratica/21-htmlwidgets.R
+++ pratica/21-htmlwidgets.R
@@ -2,8 +2,7 @@
 
 # Carregar pacotes --------------------------------------------------------
 library(shiny)
-library(shinydashboard)
-#remotes::install_github("abjur/abjData")
+library(shinydashboard)#remotes::install_github("abjur/abjData")
 
 
 # Carregando dados --------------------------------------------------------
@@ -13,20 +12,58 @@ pnud <- abjData::pnud_muni |>
 
 
 # Criar app ---------------------------------------------------------------
-ui <- shinydashboard::dashboardPage(
+ui <- dashboardPage(
     
     skin = "purple",
     
-    shinydashboard::dashboardHeader(title = "PNUD"),
+    dashboardHeader(title = "PNUD"),
     
-    shinydashboard::dashboardSidebar(
-        shinydashboard::menuItem("Informações gerais", tabName = "info")
+    dashboardSidebar(
+        menuItem("Informações gerais", tabName = "info")
     ),
     
-    shinydashboard::dashboardBody(
-        shinydashboard::tabItems(
-            shinydashboard::tabItem(
-                
+    dashboardBody(
+        tabItems(
+            tabItem(
+                tabName = "info",
+                h2("Informações gerais dos municípios"),
+                br(),
+                # linha 1 - cards ----
+                fluidRow(
+                  infoBoxOutput(
+                    outputId = "num_muni",
+                    width = 4
+                  ),
+                  infoBoxOutput(
+                    outputId = "maior_idhm",
+                    width = 4
+                  ),
+                  infoBoxOutput(
+                    outputId = "maior_idhm",
+                    width = 4
+                  )
+                ),
+                # linha 2 - outputs: gráfico e mapa----
+                fluidRow(
+                  box(
+                    width = 6,
+                    title = "Gráfico",
+                    plotly::plotlyOutput(outputId = "grafico")
+                  ),
+                  box(
+                    width = 6,
+                    title = "Mapa",
+                    leaflet::leafletOutput(outputId = "mapa")
+                  )
+                ),
+                # linha 3 - tabela ----
+                fluidRow(
+                  box(
+                    width = 12,
+                    title = "Dados",
+                    reactable::reactableOutput(outputId = "tabela")
+                  )
+                )
             )
         )
     )
